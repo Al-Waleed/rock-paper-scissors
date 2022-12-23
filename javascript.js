063c8681ab259adde1fe81b985bb566eaab253af
waleed <waleedtariq47@yahoo.com>
2022-12-23 13:38:12 +0300

    Add user input function

---
+++ javascript.js
@@ -0,0 +1,7 @@
+//create a function to ask the user for an input and converts it to lower case
+
+function getUserChoice(){   
+    let choice = prompt("Choose rock, paper or scissors").toLowerCase();  
+    return choice;
+}
+
