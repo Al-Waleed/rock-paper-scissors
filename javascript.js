9788cc67aa3aa8e8aa6154a9708331ff0c301b97
waleed <waleedtariq47@yahoo.com>
2022-12-23 13:41:41 +0300

    Add a function that plays 1 round and returns the result

--- javascript.js
+++ javascript.js
@@ -11,3 +11,24 @@ function getComputerSelection(){
     const selections = ["rock", "paper", "scissors"];    
     return selections.at(Math.floor(Math.random() * 3));
 }
+
+//create a function that gets the result of 1 round of the game
+function getRound(user,computer){
+    if (user === computer){
+        return ("it's a tie");
+    }else if (user === "rock" && computer === "paper") {
+        return ("you lose");
+    }else if (user === "rock" && computer === "scissors") {
+        return ("you win");
+    }else if (user === "paper" && computer === "rock") {
+        return ("you win");
+    }else if (user === "paper" && computer === "scissors") {
+        return ("you lose");
+    }else if (user === "scissors" && computer === "rock") {
+        return ("you lose");
+    }else if (user === "scissors" && computer === "paper") {
+        return ("you win");
+    }else {
+    return ("not a valid choice");
+}
+}
